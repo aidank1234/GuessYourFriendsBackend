d3c4be3262c84a6aec45e26adfc20418f69e75ef
aidankaiser <apkaiser@wisc.edu>
2021-03-19 10:46:32 -0500

    Shell of REST API using Node.js express mongodb

---
+++ app/models/index.js
@@ -0,0 +1,12 @@
+const dbConfig = require("../db.config.js");
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.mongoose.set('useCreateIndex', true);
+db.mongoose.set('useFindAndModify', false);
+db.url = dbConfig.url;
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jsonwebtoken = require("jsonwebtoken");
+
+const app = express();
+
+let corsOptions = {
+    origin: "*"
+};
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// add user to request to authenticate
+app.use(function(req, res, next) {
+    if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+        jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'GYFServer', function(err, decode) {
+            if(err) req.user = undefined;
+            req.user = decode;
+            next();
+        });
+    }
+    else {
+        req.user = undefined;
+        next();
+    }
+});
+
+const db = require('./app/models');
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch(err => {
+        console.log("Cannot connect to database", err);
+        process.exit();
+    });
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT + '.') ;
+});
